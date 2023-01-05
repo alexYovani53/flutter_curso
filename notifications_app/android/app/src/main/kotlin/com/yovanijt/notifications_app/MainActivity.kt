389d1ed831a03e7be449597bab57b8341ac1e155
aTomas <atomas@universales.com>
2023-01-04 18:34:58 -0600

    Iniciando pantallas y cambiando id de app

--- notifications_app/android/app/src/main/kotlin/com/yovanijt/notifications_app/MainActivity.kt
+++ notifications_app/android/app/src/main/kotlin/com/yovanijt/notifications_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.notifications_app
+package com.yovanijt.notifications_app
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ notifications_app/lib/pages/message_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MessagePage extends StatelessWidget {
+  const MessagePage ({super.key});
+
+  static const routeName = "message_page"; 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
